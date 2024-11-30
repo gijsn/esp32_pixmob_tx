40d76088c3aaf024ce3df624e6648e2ecd97fd07
gijsn <61279464+gijsn@users.noreply.github.com>
2024-11-30 20:18:18 +0100

    rework to include library

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,8 @@
 // include the library
 #include <RadioLib.h>
 
+#include "pixmob_cement.h"
+
 void printTableHeader();
 void printPrompt();
 // clock out signal DIO!
@@ -98,6 +100,15 @@ void ByteArraySend(void) {
     delayMicroseconds(BitDuration);
 }
 
+Pixmob batch;  // create the pixmob instance
+
+void activeDelay(uint16_t time) {
+  for (uint16_t i = 0; i < (time / 40); i++) {
+    delay(40);
+    batch.refresh();
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   pinMode(RADIO_DIO_2_PORT, OUTPUT);
@@ -105,8 +116,6 @@ void setup() {
   // SPI.setMOSI(RADIO_MOSI_PORT);
   // SPI.setMISO(RADIO_MISO_PORT);
   // SPI.setSCLK(RADIO_SCLK_PORT);
-  radio.setOOK(true);
-  radio.transmitDirect();
 
   // initialize SX1276 with default settings
   Serial.print(F("[SX1276] Initializing ... "));
@@ -139,16 +148,72 @@ void setup() {
     Serial.println(state);
   }
   Serial.println("done");
+
+  if (!batch.begin(RADIO_DIO_2_PORT)) {
+    Serial.println("CC1101 init fail");
+    while (true);                             // loop forever
+  } else {
+    Serial.println("CC1101 init done");
+    delay(1000);
+  }
+  Serial.println("starting red");
+  batch.storeColorSilent(128, 0, 0, 0);       // red
+  activeDelay(80);                            //  repeat sending 2 times
+  Serial.println("starting orange");
+  batch.storeColorSilent(96, 32, 0, 1);       // orange
+  activeDelay(80);                            //  repeat sending 2 times
+  Serial.println("starting yellow");
+  batch.storeColorSilent(64, 64, 0, 2);       // yellow
+  activeDelay(80);                            //  repeat sending 2 times
+  batch.storeColorSilent(32, 96, 0, 3);       // light green
+  activeDelay(80);                            //  repeat sending 2 times
+  batch.storeColorSilent(0, 128, 0, 4);       // green
+  activeDelay(80);                            //  repeat sending 2 times
+  batch.storeColorSilent(0, 96, 32, 5);       // cold green
+  activeDelay(80);                            //  repeat sending 2 times
+  batch.storeColorSilent(0, 64, 64, 6);       // turkis
+  activeDelay(80);                            //  repeat sending 2 times
+  batch.storeColorSilent(0, 32, 96, 7);       // aqua
+  activeDelay(80);                            //  repeat sending 2 times
+  batch.storeColorSilent(0, 0, 128, 8);       // blue
+  activeDelay(80);                            //  repeat sending 2 times
+  batch.storeColorSilent(32, 0, 96, 9);       // warm blue
+  activeDelay(80);                            //  repeat sending 2 times
+  batch.storeColorSilent(64, 0, 64, 10);      // violett
+  activeDelay(80);                            //  repeat sending 2 times
+  batch.storeColorSilent(96, 0, 32, 11);      // purple
+  activeDelay(80);                            //  repeat sending 2 times
+  batch.storeColorSilent(96, 64, 10, 12);     // warm white
+  activeDelay(80);                            //  repeat sending 2 times
+  batch.storeColorSilent(40, 40, 40, 13);     // cold white
+  activeDelay(80);                            //  repeat sending 2 times
+  batch.storeColorSilent(0, 0, 0, 14);        // black
+  activeDelay(80);                            //  repeat sending 2 times
+  batch.storeColorSilent(255, 255, 255, 15);  // full on
+  activeDelay(80);                            //  repeat sending 2 times
+
+  batch.setFXtiming(7, 6, 0, 0);              // slowest attack, longest hold, be background for fading, no random
+  batch.playMemForever(0, 11, 0);             // fade color 0 to color 11
+  activeDelay(80);                            //  repeat sending 2 times
 }
 
+#define RED 128                               // 0 - 255
+#define GREEN 30                              // 0 - 255
+#define BLUE 0                                // 0 - 255
+#define ATTACK 1                              // 0 -  7 (0 = fast)
+#define HOLD 2                                // 0 -  7 (7 = forever)
+#define RELEASE 2                             // 0 -  7 (0 = background color)
+#define RANDOM 2                              // 0 -  7 (0 = no random)
+#define GROUP 0                               // 0 - 31 (0 = all batches)
+
 void loop() {
-  ByteArray = {0xaa, 0xaa, 0x55, 0xa1, 0x21, 0x21, 0x21, 0x18, 0x8d, 0xa1, 0x0a, 0x40};
-  ByteArraySend();
-  if (millis() - Timestamp > 2000) {
-    Timestamp = millis();
-    for (int i = 0; i < BytesCount; i++)
-      ByteArray[i] = ColorArrayArray[ColorIndex][i];
-    ByteArraySend();
-    ColorIndex = ColorIndex++ == ValidValuesCount - 1 ? 0 : ColorIndex;
-  }
+  return;
+  batch.setFXtiming(0, 0, 0, 0);
+  batch.sendColor(0, 0, 0, 0);
+  // activeDelay(5000);
+  batch.setFXtiming(ATTACK, HOLD, RELEASE, RANDOM);
+  batch.sendColor(RED, GREEN, BLUE, GROUP);
+  activeDelay(5000);
+  batch.flashDual(128, 0, 0, 0, 0, 128);
+  activeDelay(2000);
 }
